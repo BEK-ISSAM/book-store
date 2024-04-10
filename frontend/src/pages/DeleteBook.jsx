cda5975773b637f32a02d2c08577d228dd4daff0
Issam Bekkari <bekkariissam1999@gmail.com>
2024-04-10 05:09:04 +0000

    Finished working on the book store project

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const DeleteBook = () => {
-  return (
-    <div>DeleteBook</div>
-  )
-}
+  const [loading, setLoading] = useState();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [bookName, setBookName] = useState("");
+  const { enqueueSnackbar } = useSnackbar();
 
-export default DeleteBook
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBookName(response.data.title);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleDeleteBook = () => {
+    setLoading(false);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Book deleted successfully", { variant: "success" });
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        enqueueSnackbar("Error deleting Book", { variant: "error" });
+        console.log(error);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">
+          Are You Sure you want to delete the book "{bookName}"?
+        </h3>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteBook;
